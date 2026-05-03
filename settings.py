5ba33f39041d81fc7ce10ac7f172952bbc792fd9
Arghya Nath <natharghya6294@gmail.com>
2026-05-03 14:51:02 +0000

    initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings():
+    def __init__(self):
+        self.screen_height = 800
+        self.screen_width = 1200
+        self.bg_color = (28,37,60)
+
+        self.ship_speed_factor = 1.5
+
+        self.bullet_speed_factor = 1
+        self.bullet_height = 15
+        self.bullet_wideth = 3
+        self.bullet_color = (255,87,87)
+        self.bullets_allowed = 3
